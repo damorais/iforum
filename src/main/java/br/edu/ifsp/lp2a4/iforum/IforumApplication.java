ff27241d712c7131033232b4135bbd806c63cd8c
Daniel Morais <damorais@gmail.com>
2019-10-30 11:44:58 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/ifsp/lp2a4/iforum/IforumApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.lp2a4.iforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/lp2a4/iforum/IforumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.lp2a4.iforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
